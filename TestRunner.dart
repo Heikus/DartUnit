05713d496423e223ead6b497c921103b2dcd02d2
masaru_b_cl / TAKANO Sho <masaru.b.cl@gmail.com>
2011-10-17 14:19:22 +0900

    initial import.

---
+++ TestRunner.dart
@@ -0,0 +1,76 @@
+/*
+class FizzBuzz {
+  speak(n) {
+    if (n == 3) return 'Fizz';
+    return n.toString();
+  }
+}
+
+main() {
+  var runner = new TestRunner();
+  var fizzBuzz;
+
+  runner.setUp = () => fizzBuzz = new FizzBuzz();
+  runner.tearDown = () => fizzBuzz = null;
+
+  runner.add('speak number By 1', () => Expect.equals('1', fizzBuzz.speak(1)));
+  runner.add('speak number By 2', () => Expect.equals('2', fizzBuzz.speak(2)));
+  runner.add('speak Fizz By 3', () => Expect.equals('Fizz', fizzBuzz.speak(3)));
+  runner.add('speak Fizz By 6', () => Expect.equals('Fizz', fizzBuzz.speak(6)));
+
+  runner.run();
+}
+*/
+
+class TestRunner {
+  // Set Up
+  Function _setUp;
+  set setUp(Function func) => _setUp = func;
+
+  // Tear Down
+  Function _tearDown;
+  set tearDown(Function func) => _tearDown = func;
+
+  // Test Cases
+  Map<String, Function> tests;
+
+  // Constructor
+  TestRunner() =>
+    ((){
+      _setUp = null;
+      _tearDown = null;
+      tests = new LinkedHashMap<String, Function>();
+    })();
+
+  // Add Test Case
+  add(message, test) =>
+    tests[message] = test;
+
+  // Run Test Cases
+  run() =>
+    ((){
+      var cnt = 0;
+      var failures = 0;
+      var errors = 0;
+
+      tests.forEach((message,test) {
+        cnt++;
+        try {
+          if (_setUp != null) _setUp();
+          test();
+          print(message + ' : Green!');
+        } catch (ExpectException e) {
+          print(message + ' : Red!');
+          print('  ' + e);
+          failures++;
+        } catch (Exception e) {
+          print(message + ' : Error!');
+          print('  ' + e);
+          errors++;
+        } finally {
+          if (_tearDown != null) _tearDown();
+        }
+      });
+      print('Tests run: ${cnt}, Failures ${failures}, Errors: ${errors}');
+    })();
+}
